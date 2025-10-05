413abf61236b380f06647614258d95aa444dc60f
Isaque Campos <isaquecampos2@gmail.com>
2025-10-05 17:18:35 -0300

    Add files via upload

---
+++ Calaculadora de soma.py
@@ -0,0 +1,10 @@
+
+# Variáveis
+numero1 = 3
+numero2 = 5
+
+# Cálculo
+soma = numero1 + numero2
+
+# Exibição
+print("A soma é:", soma)
---
+++ Calculadora de volume.py
@@ -0,0 +1,9 @@
+
+cpt = 15
+lg = 9
+alt = 20
+
+
+volume = cpt * lg * alt
+
+print("O volume da caixa é:", volume, "cm³")
